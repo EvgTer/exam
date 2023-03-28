bb5e2ae4d58bb2c2ace9d0aca3fe49364928ec00
Evgeniy Tereshkin <j.teresckhin@gmail.com>
2023-03-29 01:19:11 +0300

    Released class Game

---
+++ game.cpp
@@ -0,0 +1,46 @@
+#include "game.h"
+
+Game::Game()
+{
+
+}
+
+Game::Game(QString name, float price, QString publisher)
+{
+
+}
+
+QString Game::getName() const
+{
+
+}
+
+float Game::getPrice() const
+{
+
+}
+
+QString Game::getPublisher() const
+{
+
+}
+
+void Game::setName(QString name)
+{
+
+}
+
+void Game::setPrice(float price)
+{
+
+}
+
+void Game::setPublisher(QString publisher)
+{
+
+}
+
+const Game::operator <(const Game) const
+{
+
+}
