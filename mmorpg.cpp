081236f925c37094095d5199d71860c2393c9812
Evgeniy Tereshkin <j.teresckhin@gmail.com>
2023-03-30 03:46:19 +0300

    Class MMORPG modified!

--- mmorpg.cpp
+++ mmorpg.cpp
@@ -1,26 +1,30 @@
 #include "mmorpg.h"
 
 MMORPG::MMORPG()
+    :Game()
 {
-
+    this->rating = 0;
 }
 
 MMORPG::MMORPG(QString name, float price, QString publisher, int rating)
+    :Game(name,price,publisher)
 {
-
+    this->setRating(rating);
 }
 
 void MMORPG::setRating(int rating)
 {
-
+    if(rating <= 0)
+        throw "Rating is incorrect!";
+    this->rating = rating;
 }
 
 int MMORPG::getRating() const
 {
-
+    return rating;
 }
 
 QString MMORPG::getGenre() const
 {
-
+    return "MMORPG";
 }
--- mmorpg.h
+++ mmorpg.h
@@ -5,8 +5,21 @@
 
 class MMORPG : public Game
 {
+protected:
+    int rating;
 public:
     MMORPG();
+    MMORPG(
+            QString name,
+            float price,
+            QString publisher,
+            int rating
+            );
+
+    void setRating(int rating);
+    int getRating()const;
+
+    QString getGenre()const override;
 };
 
 #endif // MMORPG_H
