85bd1042a53cc15a6f1a0122d3fcf417c92937a0
Evgeniy Tereshkin <j.teresckhin@gmail.com>
2023-03-31 18:20:47 +0300

    Add logo

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -6,6 +6,7 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    ui->label_logo->setStyleSheet("image: url(:/logo/GGStore_LOGO.jpg);");
 }
 
 MainWindow::~MainWindow()
