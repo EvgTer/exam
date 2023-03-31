a7ec78ad9f5f1291180f26d8baf788ce3a001ca8
Evgeniy Tereshkin <j.teresckhin@gmail.com>
2023-03-31 18:56:12 +0300

    Add balance to class user

--- user.cpp
+++ user.cpp
@@ -5,18 +5,35 @@
 User::User()
     :Account()
 {
-
+    balance = 0;
 }
 
-User::User(QString login, QString password)
+float User::getBalance() const
+{
+    return balance;
+}
+
+void User::addBalance(float money)
+{
+    if(money <= 0)
+        throw "Money you want to add is incorrect!";
+    this->balance += money;
+}
+
+User::User(QString login, QString password, float balance)
     :Account(login,password)
 {
-
+    this->balance = balance;
 }
 
 void User::addGame(Game* obj)
 {
+    if(balance -= obj->getPrice() < 0)
+        throw "You haven`t got enough money to buy this game!";
+    else{
+    balance -= obj->getPrice();
     library.push_back(obj);
+    }
 }
 
 void User::sortLibrary()
--- user.h
+++ user.h
@@ -8,13 +8,19 @@
 
 class User : public Account
 {
+    float balance;
     QVector<Game*> library;
 public:
     User();
     User(
         QString login,
-        QString password
+        QString password,
+        float balance
         );
+
+    float getBalance()const;
+    void addBalance(float money);
+
     void addGame(Game* obj);
     void sortLibrary();
 };
